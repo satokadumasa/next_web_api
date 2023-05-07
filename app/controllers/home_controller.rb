a4f47fa58728fc8f4e3bd7b1d655bb56ee652a72
satokadumasa <sato.kadumasa@gmail.com>
2023-05-07 11:58:27 +0900

    TOPページに更新情報を表示する様に修正(2)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
     board_comments = BoardComment.eager_load(:user).all.page(1).per(5)
     @board_comments = board_comments.as_json(:include => :user)
     @pages = Page.eager_load(:user).all.page(1).per(5)
-    @boards = Board.eager_load(:user).all.order(:comment_added_at => :desc).page(1).per(5)
-    @notes = Note.eager_load(:user).all.order(:page_added_at => :desc).page(1).per(5)
+    @boards = Board.order(comment_added_at: :asc).eager_load(:user).all.page(1).per(5)
+    @notes = Note.order(page_added_at: :asc).eager_load(:user).all.page(1).per(5)
     render json: {boards: @boards, notes: @notes, pages: @pages, board_comments: @board_comments}
   end
 end
